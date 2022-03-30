e1914395e9d6bc7331699bff2f70616911ba2eb1
Jonas <jonasmooney2@gmail.com>
2022-03-30 09:25:30 -0600

    gif container building blocks

--- js/trending.mjs
+++ js/trending.mjs
@@ -1,10 +1,13 @@
 
   const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
   document.addEventListener('DOMContentLoaded', getData);
+  let gifContainer = document.querySelector('#gifContainer');
 
 
 
 let gifsHTML = '';
 gifsHTML += `<div class='row align-items-start'></div>`
 
-
+let h1 = document.createElement('h1');
+h1.append('AHHHHHHHHHH');
+gifContainer.appendChild(h1);
