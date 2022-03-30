79384e8187a5124daaeb5cff4a43983e5b5ef299
Jonas <jonasmooney2@gmail.com>
2022-03-30 09:00:45 -0600

    trending_progress

--- js/trending.mjs
+++ js/trending.mjs
@@ -1,4 +1,10 @@
 
+  const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
+  document.addEventListener('DOMContentLoaded', getData);
+
+
 
 let gifsHTML = '';
 gifsHTML += `<div class='row align-items-start'></div>`
+
+
