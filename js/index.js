e9c15764296c7786ec4d040e57e86d6ce440b2bb
Jonas <jonasmooney2@gmail.com>
2022-03-31 08:14:02 -0600

    Gif container

---
+++ js/index.js
@@ -0,0 +1,1 @@
+import {trendingGifs} from 'trending.mjs';
--- js/trending.mjs
+++ js/trending.mjs
@@ -1,13 +1,21 @@
 
-  const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
-  document.addEventListener('DOMContentLoaded', getData);
-  let gifContainer = document.querySelector('#gifContainer');
+  export function trendingGifs() {
+    const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
+    // document.addEventListener('DOMContentLoaded', getData);
+  
+  
+  
+  
+    let gifContainer = document.querySelector('#gifContainer');
+  
+  // let gifsHTML = '';
+  // gifsHTML += `<div class='row align-items-start'></div>`
+  
+  // let h1 = document.createElement('h1');
+  // h1.append('AHHHHHHHHHH');
+  // gifContainer.appendChild = h1;
+    console.log('asdf')
 
 
 
-let gifsHTML = '';
-gifsHTML += `<div class='row align-items-start'></div>`
-
-let h1 = document.createElement('h1');
-h1.append('AHHHHHHHHHH');
-gifContainer.appendChild(h1);
+  }
