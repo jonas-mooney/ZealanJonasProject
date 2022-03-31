806a6cc8f1efc85a31887a794bf6f096e225d84d
jonas-mooney <96934456+jonas-mooney@users.noreply.github.com>
2022-03-31 10:29:21 -0600

    Jonas (#14)
    * Deleted blah
    
    * Blah deletion and scss files
    
    * scss work
    
    * trending_progress
    
    * error_fix_2
    
    * gif container building blocks
    
    * Up to date
    
    * Gif container
    
    * Gif Container Trending API call
    
    * Gifs loaded
    
    Co-authored-by: Jonas <jonasmooney2@gmail.com>

--- js/trending.mjs
+++ js/trending.mjs
@@ -1,13 +1,40 @@
+const limit = 10;
+const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
 
-  const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
-  document.addEventListener('DOMContentLoaded', getData);
-  let gifContainer = document.querySelector('#gifContainer');
+document.addEventListener('DOMContentLoaded', trendingGifs);
+function trendingGifs() {
+    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}`
+    let contentContainer = document.querySelector('#content');
+    let contentHeader = document.querySelector('#contentHeader');
+    let gifContainer = document.querySelector('#gifContainer');
+    contentHeader.innerHTML = `<h1>Search and Save your Favorite Gifs</h1>`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(content => {
+      console.log(content.data)
+      render(content.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+    function render(data) {
+      for (let i=0; i<=data.length; i++) {
+        console.log(data[i].images.downsized.url);
+        gifContainer.innerHTML += `<img src='${data[i].images.downsized.url}'></img>`
+      }
+    }
+}
+ 
 
 
-
-let gifsHTML = '';
-gifsHTML += `<div class='row align-items-start'></div>`
-
-let h1 = document.createElement('h1');
-h1.append('AHHHHHHHHHH');
-gifContainer.appendChild(h1);
+function search() {
+  document.querySelector('#searchButton').addEventListener('click', ev => {
+    ev.preventDefault();
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=${limit}&q=`;
+    let str = document.querySelector('#searchInput').value.trim();
+    url = url.concat(str);
+    console.log(url);
+  })
+}
