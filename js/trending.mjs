e34dfb65206b17a35013cb522463a933b18d5eee
Jonas <jonasmooney2@gmail.com>
2022-04-01 08:12:18 -0600

    Search function

--- js/trending.mjs
+++ js/trending.mjs
@@ -2,11 +2,12 @@ const limit = 10;
 const apiKey = 'KkQIVU7CgUTlND28O2bDZveA3Z8Vl1kz';
 
 document.addEventListener('DOMContentLoaded', trendingGifs);
+const contentContainer = document.querySelector('#content');
+const contentHeader = document.querySelector('#contentHeader');
+const gifContainer = document.querySelector('#gifContainer');
+
 function trendingGifs() {
     let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}`
-    let contentContainer = document.querySelector('#content');
-    let contentHeader = document.querySelector('#contentHeader');
-    let gifContainer = document.querySelector('#gifContainer');
     contentHeader.innerHTML = `<h1>Search and Save your Favorite Gifs</h1>`;
 
     fetch(url)
@@ -21,7 +22,7 @@ function trendingGifs() {
 
     function render(data) {
       for (let i=0; i<=data.length; i++) {
-        console.log(data[i].images.downsized.url);
+        // console.log(data[i].images.downsized.url);
         gifContainer.innerHTML += `<img src='${data[i].images.downsized.url}'></img>`
       }
     }
@@ -29,12 +30,16 @@ function trendingGifs() {
  
 
 
-function search() {
+
   document.querySelector('#searchButton').addEventListener('click', ev => {
     ev.preventDefault();
     let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=${limit}&q=`;
     let str = document.querySelector('#searchInput').value.trim();
     url = url.concat(str);
+    gifContainer.innerHTML = null;
+    contentHeader.innerHTML = `<h1>${document.querySelector('#searchInput').value}</h1>`;
+    for (let i=0; i<=data.length; i++) {
+      gifContainer.innerHTML += `<img src='${data[i].images.downsized.url}'></img>`
     console.log(url);
+    }
   })
-}
